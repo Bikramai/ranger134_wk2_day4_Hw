32760a3e20616cfe2cac6869013a4c3aa97c04e9
bikramia <bikram.rhce@gmail.com>
2023-10-20 09:36:48 -0400

    second

---
+++ my_module.py
@@ -0,0 +1,11 @@
+def cal_square_footage(Length, Width, Area):
+# """Calculate the square footage of a house.
+#
+#Args:
+#   1. length: The lenth of the house in feet.
+#   2. width: The width of the house in feet
+#
+# Returns:
+#   The square footage of the house in square ft.
+# """
+    return Length * Width == Area
---
+++ my_module1.py
@@ -0,0 +1,10 @@
+def cal_circumference(radius, r, math):
+# """calculates the circumference of a circle.
+# 
+# Args:
+    # radius: The radius of the a circle in ft.
+
+# Returns:
+#   The circumference of the circle in ft.
+# """
+    return 2*math.pi*radius
